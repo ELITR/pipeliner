5d8d255e15e69b3d6ee1a17c9158ee9a48654000
ELITR notebook Grappler (Ondrej or others) <bojar@ufal.mff.cuni.cz>
2021-02-19 11:06:13 +0100

    do not tail -F by default

--- src/pipeliner.py
+++ src/pipeliner.py
@@ -209,7 +209,11 @@ mkdir -p {self.logsDir}
     commands += self._createPipes()
 
     componentCount = len(self.graph.nodes)
-    commands += [f"tail -F -n {componentCount} {self.logsDir}/*.err"]
+    commands += [f"echo '#' Container $(hostname) is running the pipeline. Kill container with ctrl-D."]
+    commands += [f"echo '#' Logs are in: {self.logsDir}"]
+    commands += [f"echo '#' Watch them using:"]
+    commands += [f"echo '  ' tail -F -n {componentCount} {self.logsDir}/'*'.err"]
+    commands += [f"echo '  ' tail -F -n {componentCount} {self.logsDir}/'*'.err"]
     self._prologue()
     self._reportEntrypoints()
     print(" &\n".join(commands))
