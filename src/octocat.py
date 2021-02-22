8a1ea819ea75852fcf339ef088a8c775d89d74b2
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2021-02-23 00:14:11 +0100

    read SELECT only upon change

--- src/octocat.py
+++ src/octocat.py
@@ -114,17 +114,30 @@ def load_inputs():
             logging.debug(f'input {f}: socket on port {port}')
     return inputs
 
-def read_select(inputs):
-    try:
-        s = open('SELECT').readline().strip()
-        return inputs[s]
-    except:
-        logging.error(f'invalid entry in SELECT; valid entries: {list(inputs.keys())}')
-        return list(inputs.items())[0][1]
+class Selector:
+    def __init__(self):
+        self.select_last_read_stamp = 0
+        self.select_last_read = None
+
+    def read_select(self, inputs):
+        try:
+            stamp = os.stat('SELECT').st_mtime
+            if self.select_last_read_stamp == 0 or stamp != self.select_last_read_stamp:
+                s = open('SELECT').readline().strip()
+                selected = inputs[s]
+                self.select_last_read_stamp = stamp
+                self.select_last_read = selected
+                logging.info(f'read SELECT, will now follow {s} ({selected})')
+                return inputs[s]
+            else:
+                return self.select_last_read
+        except:
+            logging.error(f'invalid entry in SELECT; valid entries: {list(inputs.keys())}')
+            return self.select_last_read if self.select_last_read is not None else list(inputs.items())[0][1]
 
 def main(args):
     root = logging.getLogger()
-    root.setLevel(logging.DEBUG if args.debug else logging.ERROR)
+    root.setLevel(logging.DEBUG if args.debug else logging.INFO)
 
     handler = logging.StreamHandler(sys.stderr)
     handler.setLevel(logging.DEBUG)
@@ -137,7 +150,8 @@ def main(args):
     for _, input in inputs.items():
         input.start()
 
-    select = read_select(inputs)
+    selector = Selector()
+    select = selector.read_select(inputs)
     select.read = True
     select2 = None
     while True:    
@@ -159,7 +173,7 @@ def main(args):
                         break
                 if time.time() - start > args.interval:
                     logging.debug(f'read {read_len} bytes during last {time.time() - start} seconds')
-                    select2 = read_select(inputs)
+                    select2 = selector.read_select(inputs)
                     if select2 != select:
                         select.read = False
                         select2.read = True
