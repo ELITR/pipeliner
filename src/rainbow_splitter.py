6a6cad87abc1423ec5c701ce803b6670f36218a3
Ondrej Bojar <bojar@ufal.mff.cuni.cz>
2021-02-24 11:38:08 +0100

    we *have* to be paranoid; fixed excessive newline

--- src/rainbow_splitter.py
+++ src/rainbow_splitter.py
@@ -1,16 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # Usage; ./rainbow-splitter.py [LIST_OF_LANGS] [LIST_OF_PORTS]
 # Splits a rainbow MT packet into individual languages , outputting on ports
 import sys
 import math
 import socket
 
+import sys
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 args = sys.argv[1:]
 half = len(args) // 2
 langs = args[:half]
-ports = map(lambda x: int(x), args[half:])
+ports = list(map(lambda x: int(x), args[half:]))
 
 sockets = {}
+
+eprint("rainbow-splitter starting")
+eprint("Expected to receive langs:    ", langs)
+eprint("Expected to receive on ports: ", ports)
+langsset = " ".join(sorted(langs))
+
+assert len(langs) == len(ports), f"Mismatched lengths of langs and ports: {len(langs)} vs {len(ports)}"
 
 for lang, port in zip(langs, ports):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,10 +30,16 @@ for lang, port in zip(langs, ports):
     sockets[lang] = s
 
 for line in sys.stdin:
+    line = line.rstrip("\r\n")
     timestamp = " ".join(line.split(" ")[:2])
+      # the timestamp are the first two words on the line
     packets = line.split("\t")
-    packets[0] = packets[0].split(" ")[2] # Strip the timestamp from the first column
-    pairs = zip(packets[0::2], packets[1::2])
+    packets[0] = packets[0].split(" ")[2]
+      # strip the timestamp from the first column
+    gotlangs = packets[0::2]
+    gotlangsset = " ".join(sorted(gotlangs))
+    assert langsset == gotlangsset, f"Mismatched set of languages:\nEXP: {langsset}\nGOT: {gotlangsset}"
+    pairs = zip(gotlangs, packets[1::2])
     for lang, sentence in pairs:
         if lang in langs:
             print(sentence)
